af41c53339d8ed36054908c17ec95936c0fd5b65
Suchismita13 <143961796+Suchismita13@users.noreply.github.com>
2024-03-19 03:10:24 +0530

    update task1 code

--- task1.java
+++ task1.java
@@ -1,49 +1,47 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
-class task1{
-     
-        public static void main(String[] args) 
-       {
-           System.out.println("Let's play a game.  I'll pick a number between");
-           System.out.println("1 and 100, and you try to guess it.");
-           Scanner sc1 = new Scanner(System.in); 
-           boolean playAgain;
-           do {
-              playGame();  // call subroutine to play one game
-              System.out.println("Would you like to play again?Y/N");
-              playAgain = sc1.nextBoolean();  
-              } while (playAgain);
-          System.out.println("Thanks for playing.Goodbye.");
-        }         
-        
-        static void playGame() {
-            int computersNumber; 
-            int usersGuess;      
-            int guessCount;      
-            computersNumber = (int)(100 * Math.random()) + 1;
-            guessCount = 0;
-            System.out.println();
-            System.out.println("What is your first guess?");
-            Scanner sc= new Scanner(System.in);   
-            while (true) {
-               usersGuess = sc.nextInt();  
-               guessCount++;
-               if (usersGuess == computersNumber) {
-                  System.out.println("You got it in " + guessCount
-                          + " guesses!  My number was " + computersNumber);
-                  break;  
-               }
-               if (guessCount == 6) {
-                  System.out.println("You didn't get the number in 6 guesses.");
-                  System.out.println("You lose.  My number was " + computersNumber);
-                  break;  
-               }
-               if (usersGuess < computersNumber)
-                  System.out.println("That's too low.  Try again:");
-               else if (usersGuess > computersNumber)
-                  System.out.println("That's too high.  Try again:");
-            }
-            System.out.println();
-        } 
-                    
-     } 
+public class task1 {
+public static void game()
+	{
+	Scanner sc = new Scanner(System.in);
+   int number = 1 + (int)(100* Math.random());
+   int K = 5,i, guess;
+
+		System.out.println("choose a number");
+
+		
+		for (i = 0; i < K; i++) {
+
+			System.out.println("Guess the number:");
+
+			
+			guess = sc.nextInt();
+
+			
+			if (number == guess) {
+				System.out.println("guess is correct");
+				break;
+			}
+			else if (number > guess && i != K - 1) {
+				System.out.println("too high");
+			}
+			else if (number < guess && i != K - 1) {
+				System.out.println("too low");
+			}
+		}
+
+		if (i == K) {
+			System.out.println("You have exhausted" + " K trials.");
+
+			System.out.println("number " + number);
+		}
+	}
+
+	
+	public static void
+	main(String arg[])
+	{
+
+		game();
+	}
+}
